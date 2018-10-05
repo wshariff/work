d83a66de1e5b99590a4d73121c1e43f7601be27f
Waheeda Shariff <wshariff2468@googlemail.com>
2018-10-05 16:59:45 +0100

    Program use

--- Mini Project Jurassic World.java
+++ Mini Project Jurassic World.java
@@ -3,7 +3,7 @@
 
    public class jurassic {
        public static void main ( String [] args {
-        
+       System.out.println("This is a program that stimulates bio-engineered dinosaurs who need you to look after them."); 
 
 
         
@@ -16,5 +16,5 @@
         System.out.println("Please name the species of the dinosaur you've made.");
         dinosaur1 = newname1.next( );
         System.out.println("Please run away from Waheeda, she scary...");
-        System.out
+        System.out.println ("Too late, you can run but you can't hide!")
    }
