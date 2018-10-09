5ff69564b0b22e7932e00f0764a634e0b9cb532e
wshariff <43821666+wshariff@users.noreply.github.com>
2018-10-09 12:40:46 +0100

    Implemented message to user using name of dinosaur

--- Mini Project Jurassic World.java
+++ Mini Project Jurassic World.java
@@ -3,18 +3,15 @@
 
    public class jurassic {
        public static void main ( String [] args {
-       System.out.println("This is a program that stimulates bio-engineered dinosaurs who need you to look after them."); 
-
-
-        
+       System.out.println("This is a program that stimulates bio-engineered dinosaurs who need you to look after them.");
 
        }
 
        
         Scanner newname1 = new Scanner( System.in );
         String dinosaur1;
-        System.out.println("Please name the species of the dinosaur you've made.");
+        System.out.println("Please name the species of your dinosaur.");
         dinosaur1 = newname1.next( );
-        System.out.println("Please run away from Waheeda, she scary...");
-        System.out.println ("Too late, you can run but you can't hide!")
+        System.out.println( dinosaur1 + " are cool. ");
+
    }
