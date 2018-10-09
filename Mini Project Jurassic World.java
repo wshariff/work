4167d2488e0b56a3a269d3d6fedc425badba102e
wshariff <43821666+wshariff@users.noreply.github.com>
2018-10-09 12:47:12 +0100

    Program is running
    import scanner 
    corrected class
    new methods

--- Mini Project Jurassic World.java
+++ Mini Project Jurassic World.java
@@ -1,17 +1,35 @@
 /* Author: Waheeda Shariff
    Date: 05/10/2018 */
+import java.util.Scanner; 
 
-   public class jurassic {
-       public static void main ( String [] args {
-       System.out.println("This is a program that stimulates bio-engineered dinosaurs who need you to look after them.");
+  class jurassic {
+       public static void main ( String [] args) {
 
-       }
 
-       
+	JurassicWorld();
+	NameSpecies();
+	System.exit(0);
+
+	}// END main
+
+	public static void JurassicWorld()
+        {
+	
+	System.out.println("This is a program that stimulates bio-engineered dinosaurs who need you to look after them.");
+	System.out.println(" ");
+	
+	return;
+        }// END JurassicWorld
+	
+	public static void NameSpecies()
+        {
+
         Scanner newname1 = new Scanner( System.in );
         String dinosaur1;
         System.out.println("Please name the species of your dinosaur.");
         dinosaur1 = newname1.next( );
         System.out.println( dinosaur1 + " are cool. ");
-
-   }
+	
+	return;
+	}// END JurassicWorld
+               }// END jurassic
