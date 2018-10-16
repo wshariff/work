d04cedc64b663183d65b1a8495e3a2542b0ee713
Waheeda Jylan Shariff <wjs30@itl100.student.eecs.qmul.ac.uk>
2018-10-16 13:02:38 +0100

    Created Eating Type

--- Mini Project Jurassic World.java
+++ Mini Project Jurassic World.java
@@ -9,6 +9,8 @@
       jurassicWorld();
       nameSpecies();
       setHungerLevel();
+      checkHungerLevel();
+			
     }// END main
 
     public static void jurassicWorld(){
@@ -18,16 +20,18 @@
   
     return;
     }// END JurassicWorld
-    
-    public static void nameSpecies(){
+
+//Ask question- what their dinosaur's eating type is? 
+//Create if statement for the eating type of the dinosaur- omnivore...
+     public static void nameSpecies(){
   
-      Scanner newname1 = new Scanner( System.in );
-      String dinosaur1;
-      System.out.println("Please name the species of your dinosaur below.");
-      dinosaur1 = newname1.next( );
-      System.out.println( dinosaur1 + " are cool. ");
-  
-      return;
+	Scanner newname1 = new Scanner( System.in );
+	String dinosaur1;
+	System.out.println("Please name the species of your dinosaur below.");
+	dinosaur1 = newname1.next( )
+	System.out.println(dinosaur1 + " are cool. ");
+
+      	return;
     }
   
     public static int setHungerLevel() {
@@ -41,6 +45,39 @@
     
     return hungerLevel;
     }
-        
+	
+	public static void checkHungerLevel(String eatingType){
+	
+	Scanner newname1 = new Scanner( System.in );
+	System.out.println("Is" + dinosaur1 " a carnivore, herbivore or omnivore?");
+	String eatingType = newname1.next( );
+
+	//hungry dinosaur needs to go hunting 
+
+	
+
+	
+	if (hungerLevels.equals (<=5) {
+		if (eatingType.contains ("carnivore") | eatingType.contains ("omnivore"))
+		{
+			System.out.println( dinosaur1 + " is hungry, so it is time to go hunting.");
+		}
+		
+		else
+		{
+			System.out.println( dinosaur1 + " is hungry, so it is time to go find plants.");
+		}
+	
+	}
+
+	// 
+	else if (hungerLevels.equals (hungerlevels >=6) {
+	
+	System.out.println( dinosaur1 + "  .");
+	}
+      /*ideas 1)dinosaur is full, can go find somewhere to go sleep or go to sleep.
+	2) interspecies rivalry or carnivore attack = dinosaur needs to defend themselves 
+	3)
+*/
   }
   
