1f691b65169ce82d4c64f302d7ccd2777a3b6db4
Waheeda Shariff <wshariff2468@googlemail.com>
2018-11-28 17:35:53 +0000

    While loop in progress

---
+++ Trains.java
@@ -0,0 +1,28 @@
+/* Author: Waheeda Shariff
+   Date: 04/10/2018 */
+
+import java.util.Scanner;
+
+public class Trains 
+{
+
+  public static void main (String[] args) 
+  {
+    Late()
+  }
+
+    public static void Late() 
+    {
+      Scanner scanner = new Scanner ( System.in );
+      int minutes = scanner.next();
+      int compare = 0; 
+      int total = 0
+
+      if 
+      
+
+
+
+    }
+  }
+
